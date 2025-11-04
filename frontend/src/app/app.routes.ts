c13ba436e10dc5b2d2dfdb89780d6471b10474ec
Andres Cardenas <andres4456cardenas@gmail.com>
2025-11-03 20:26:39 -0500

    Se agrega las vistas correspondientes a las paginas de inicio y servicios con sus respectivos estilos

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
-
+import { Inicio } from './pages/inicio/inicio';
+import { Servicios } from './pages/servicios/servicios';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
+  { path: 'inicio', component: Inicio},      
+  { path: 'servicios', component: Servicios}
 ];
